3b461e779480883a06dc1851e5f68cd177aa2e3b
Nikita Lipatov <6stringsohei@gmail.com>
2026-07-19 14:10:09 +0400

    fix(reader): bound capped storage read retries

--- reader_test.go
+++ reader_test.go
@@ -2,12 +2,15 @@ package torrent
 
 import (
 	"context"
+	"io"
 	"testing"
 	"time"
 
+	"github.com/anacrolix/torrent/bencode"
 	"github.com/stretchr/testify/require"
 
 	"github.com/anacrolix/torrent/internal/testutil"
+	"github.com/anacrolix/torrent/metainfo"
 )
 
 func TestReaderReadContext(t *testing.T) {
@@ -39,4 +42,42 @@ func TestReaderSetContextAndRead(t *testing.T) {
 	r.SetContext(ctx)
 	_, err = r.Read(make([]byte, 1))
 	require.EqualValues(t, context.DeadlineExceeded, err)
+}
+
+func TestCappedStorageReadFailuresReturn(t *testing.T) {
+	cfg := TestingConfig(t)
+	cfg.DefaultStorage = badStorage{}
+	cl, err := NewClient(cfg)
+	require.NoError(t, err)
+	defer cl.Close()
+
+	infoBytes, err := bencode.Marshal(metainfo.Info{
+		PieceLength: 100,
+		Pieces:      make([]byte, 20),
+		Files:       []metainfo.FileInfo{{Path: []string{"greeting"}, Length: 100}},
+	})
+	require.NoError(t, err)
+	tt, added := cl.AddTorrentOpt(AddTorrentOpts{
+		InfoBytes: infoBytes,
+		InfoHash:  metainfo.HashBytes(infoBytes),
+	})
+	require.True(t, added)
+	defer tt.Drop()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	r := tt.NewReader()
+	_, err = r.Seek(50, io.SeekStart)
+	require.NoError(t, err)
+	r.SetContext(ctx)
+	_, err = r.Read(make([]byte, 1))
+	require.ErrorIs(t, err, context.Canceled)
+	require.NoError(t, r.Close())
+
+	r = tt.NewReader()
+	defer r.Close()
+	_, err = r.Seek(50, io.SeekStart)
+	require.NoError(t, err)
+	_, err = r.Read(make([]byte, 1))
+	require.ErrorIs(t, err, io.ErrUnexpectedEOF)
 }
